c6f35d585db91faa8ae71ff4013d02d3c8d7d973
Victor Zuluaga <zrvictor@outlook.com>
2023-08-22 19:17:33 -0500

    first commit frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from 'flowbite-react';
+function App () {
+  return (
+    <>
+      <h1 className='text-xl'>Hello world!</h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,15 @@
+// @packages
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// @scripts
+import App from './App.jsx';
+
+// @styles
+import './styles/output.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
