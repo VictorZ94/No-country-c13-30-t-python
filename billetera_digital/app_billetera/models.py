5fe0d8d3535b9bf25da3fc0f66d665fd36e97cf4
ruizdani301 <ruizdani301@gmail.com>
2023-08-24 17:03:55 -0500

    UPD: se reorganizan los modelos

--- billetera_digital/app_billetera/models.py
+++ billetera_digital/app_billetera/models.py
@@ -1,32 +1,53 @@
 from django.db import models
 
 
-class IdentificationDetails(models.Model):
-    identification_type = models.CharField(
-        'tipo_de_identificacion', max_length=20)
-    number_id = models.CharField('numero', unique=True, max_length=20)
-    address = models.CharField('direcciòn', max_length=50)
+class Transaction(models.Model):
+    tags = models.CharField(
+        'tags', max_length=50)
+    created_at = models.DateTimeField('fecha creaciòn')
+    amount = models.IntegerField('valor')
+    details = models.CharField('detalles', max_length=200)
+    category = models.CharField('categoria', max_length=15)
 
     class Meta:
-        db_table = 'detalles_identificacion'
-        verbose_name = 'Detalles_identificacion'
-        verbose_name_plural = 'Detalles_identificacion'
-        ordering = ['number_id']
+        db_table = 'transacciones'
+        verbose_name = 'transacciones'
+        verbose_name_plural = 'transacciones'
+        ordering = ['amount']
 
     def __str__(self):
-        return f'{self.id} {self.identification_type} {self.number_id} {self.address}'
+        return f'{self.id} {self.created_at}'
+
+
+class BalanceDetails(models.Model):
+    balance = models.CharField(
+        'tags', max_length=50)
+    created_at = models.DateTimeField('fecha creaciòn')
+
+    class Meta:
+        db_table = 'balance'
+        verbose_name = 'balance'
+        verbose_name_plural = 'balance'
+        ordering = ['created_at']
+
+    def __str__(self):
+        return f'{self.id} {self.created_at}'
 
 
 class User(models.Model):
     name = models.CharField(
         'nombre', max_length=50)
+    type_id = models.CharField('tipo_identificacion', max_length=10)
+    identification = models.CharField('cedula', unique=True, max_length=15)
     cell_phone = models.CharField('celular', unique=True, max_length=10)
     email = models.EmailField('correo', unique=True, max_length=100)
     password = models.CharField('contraseña', unique=True, max_length=100)
     create_at = models.DateTimeField('fecha creaciòn', auto_now=True)
     updated_at = models.DateTimeField('fecha de actualizaciòn')
-    id_identification_details = models.ForeignKey(
-        IdentificationDetails, on_delete=models.CASCADE)
+    id_transactions = models.ForeignKey(
+        Transaction, on_delete=models.CASCADE)
+    id_balance_details = models.ForeignKey(
+        BalanceDetails, on_delete=models.CASCADE)
 
     class Meta:
         db_table = 'usuario'
@@ -35,66 +56,4 @@ class User(models.Model):
         ordering = ['name']
 
     def __str__(self):
-        return f'{self.id} {self.company_name} {self.reference_number}'
-
-
-class Withdrawal(models.Model):
-    pass
-
-    class Meta:
-        db_table = 'retiro'
-        verbose_name = 'retiro'
-        verbose_name_plural = 'retiro'
-        #ordering = ['']
-
-    def __str__(self):
-        return f'{self.id}'
-
-
-class PaymentTransfer(models.Model):
-    number_id = models.CharField(
-        'numero de cedula', unique=True, blank=False, null=False, max_length=20)
-
-    class Meta:
-        db_table = 'trasferencia_de_pago'
-        verbose_name = 'trasferencia de pago'
-        verbose_name_plural = 'trasferencia de pago'
-        ordering = ['number_id']
-
-    def __str__(self):
-        return f'{self.id} {self.number_id}'
-
-
-class Invoice(models.Model):
-    company_name = models.CharField(
-        'nombre de la empresa', max_length=50)
-    reference_number = models.IntegerField('numero de referencia', unique=True)
-
-    class Meta:
-        db_table = 'factura'
-        verbose_name = 'Detalles_identificacion'
-        verbose_name_plural = 'factura'
-        ordering = ['reference_number']
-
-    def __str__(self):
-        return f'{self.id} {self.company_name} {self.reference_number}'
-
-
-class Payments(models.Model):
-    tags = models.CharField(
-        'tags', max_length=50)
-    created_at = models.DateTimeField('fecha creaciòn')
-    amount = models.IntegerField('valor')
-    id_user = models.ManyToManyField(User)
-    id_invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
-    id_paymenttransfer = models.ForeignKey(
-        PaymentTransfer, on_delete=models.CASCADE)
-
-    class Meta:
-        db_table = 'pago'
-        verbose_name = 'pago'
-        verbose_name_plural = 'pago'
-        ordering = ['amount']
-
-    def __str__(self):
-        return f'{self.id} {self.created_at} {self.id_user}'
+        return f'{self.id} {self.name} {self.cell_phone}'
